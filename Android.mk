billchen1977/android_kernel_xiaomi_kenzo

Build with aosp
Change-Id: I29b336d8705ec31e6632d93e1c008c256f72fc70

Android.mk
@@ -1,0 +1,18 @@
+ifeq ($(TARGET_DEVICE),kenzo)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+-include $(LOCAL_PATH)/AndroidKernel.mk
+
+LOCAL_PREBUILT_MODULE_FILE := $(TARGET_PREBUILT_KERNEL)
+
+LOCAL_MODULE       := kernel
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := KERNEL_OBJ
+LOCAL_MODULE_PATH  := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
+
+endif
+

DarkCAF-OS/android_kernel_xiaomi_hydrogen

Android.mk
@@ -1,0 +1,18 @@
+ifeq ($(TARGET_DEVICE),hydrogen)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+-include $(LOCAL_PATH)/AndroidKernel.mk
+
+LOCAL_PREBUILT_MODULE_FILE := $(TARGET_PREBUILT_KERNEL)
+
+LOCAL_MODULE       := kernel
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := KERNEL_OBJ
+LOCAL_MODULE_PATH  := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
+
+endif
+